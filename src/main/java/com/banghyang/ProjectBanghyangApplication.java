298616b58f98990f726c8af17594a13f5de198ec
DESKTOP-G0504UF\20116 <wdh970616@naver.com>
2024-11-12 12:53:50 +0900

    first commit

---
+++ src/main/java/com/banghyang/ProjectBanghyangApplication.java
@@ -0,0 +1,13 @@
+package com.banghyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBanghyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBanghyangApplication.class, args);
+    }
+
+}
