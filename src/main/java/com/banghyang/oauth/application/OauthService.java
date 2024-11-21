37d2a7c101743410c829fa1ef9269f0fe08ca7c7
DESKTOP-G0504UF\20116 <wdh970616@naver.com>
2024-11-21 09:54:26 +0900

    카카오 연동 해제 기능 작성중

--- src/main/java/com/banghyang/oauth/application/OauthService.java
+++ src/main/java/com/banghyang/oauth/application/OauthService.java
@@ -28,20 +28,18 @@ public class OauthService {
     }
 
     /**
-     *
+     * 회원가입 처리 메소드
      * @param oauthServerType
      * @param authCode
      * @return
      */
     public Long login(OauthServerType oauthServerType, String authCode) {
+        // 플랫폼명과 AuthCode 보내서 사용자 정보 받아와서 객체로 만들기
         OauthMember oauthMember = oauthMemberClientComposite.fetch(oauthServerType, authCode);
+        // 객체 저장
         OauthMember saved = oauthMemberRepository.findByOauthId(oauthMember.getOauthId())
                 .orElseGet(() -> oauthMemberRepository.save(oauthMember));
+        // 저장한 객체 아이디 반환
         return saved.getId();
-    }
-
-    public void unlinkKakao(String accessToken) {
-        String token = "Bearer " + accessToken;
-        kakaoApiClient.unlink(token);
     }
 }
