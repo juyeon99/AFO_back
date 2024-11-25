f9ee2b4af8179fe456968dc6198eea55c0127002
DESKTOP-G0504UF\20116 <wdh970616@naver.com>
2024-11-25 13:13:04 +0900

    계열 정보 저장

--- src/main/java/com/banghyang/line/entity/Line.java
+++ src/main/java/com/banghyang/line/entity/Line.java
@@ -1,5 +1,23 @@
 package com.banghyang.line.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Line {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String content;
+    private String color;
 }
