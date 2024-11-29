3c21cf6473d840a305f2ef6d2fa1a744ced39bda
kimkinghyeon <modan8199@gmail.com>
2024-11-29 17:53:43 +0900

    - MultipartFile 처리 추가 service 에서만 multipartfile 의 내용을  판별하도록 했는데 controlloer 에서도 관리하도록 수정

--- src/main/java/com/banghyang/recommend/controller/RecommendController.java
+++ src/main/java/com/banghyang/recommend/controller/RecommendController.java
@@ -2,6 +2,7 @@ package com.banghyang.recommend.controller;
 
 import com.banghyang.recommend.service.RecommendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +20,20 @@ public class RecommendController {
     public ResponseEntity<Map<String, Object>> recommendPerfume(
             @RequestParam("user_input") String userInput,
             @RequestParam(value = "image", required = false) MultipartFile image) {
+        try {
+            // 로그로 image 상태 확인
+            if (image != null) {
+                System.out.println("Image received: " + image.getOriginalFilename());
+            } else {
+                System.out.println("No image received");
+            }
 
-        Map<String, Object> response = recommendService.processInputAndImage(userInput, image);
-        return ResponseEntity.ok(response);
+            Map<String, Object> response = recommendService.processInputAndImage(userInput, image);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            e.printStackTrace(); // 예외 출력
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Internal Server Error"));
+        }
     }
 }
+
